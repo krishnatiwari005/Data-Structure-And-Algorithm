cf7faa085dee57177bbe4ce98589f1739ed71ecb
Krishna Tiwari <krishna.08072005@gmail.com>
2026-07-25 12:29:58 +0530

    Time: 1088 ms (35.33%), Space: 7.9 MB (5.16%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool checkPerfectNumber(int num) {
         int s = 0;
-        for (int i = 1; i < num; i++) {
+        for (int i = 1; i <= num / 2; i++) {
             if (num % i == 0) {
                 s += i;
             }
