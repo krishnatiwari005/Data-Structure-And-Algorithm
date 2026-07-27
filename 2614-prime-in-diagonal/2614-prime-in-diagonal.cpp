6e7bf77b6fc91c2b233799264ac9af735d11ebd5
Krishna Tiwari <krishna.08072005@gmail.com>
2026-07-27 21:40:19 +0530

    Time: 4 ms (58.36%), Space: 39.5 MB (70.77%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.cpp
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.cpp
@@ -2,11 +2,13 @@ class Solution {
 public:
     int diagonalPrime(vector<vector<int>>& nums) {
         int ans = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j < nums.size(); j++) {
-                if ((i == j || i + j == nums.size() - 1) && check(nums[i][j])) {
-                    ans = max(ans, nums[i][j]);
-                }
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            if (check(nums[i][i])) {
+                ans = max(ans, nums[i][i]);
+            }
+            if (i != n - 1 - i && check(nums[i][n - 1 - i])) {
+                ans = max(ans, nums[i][n - 1 - i]);
             }
         }
         return ans;
