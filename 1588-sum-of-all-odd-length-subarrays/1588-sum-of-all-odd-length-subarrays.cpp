ebf8c795c7cb524454c45953cbe05d6f9288fed0
Krishna Tiwari <krishna.08072005@gmail.com>
2026-08-03 14:02:11 +0530

    Time: 0 ms (100%), Space: 11.2 MB (56.2%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.cpp
@@ -4,13 +4,9 @@ public:
         int ans = 0;
         int n = arr.size();
         for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if ((i - j + 1) % 2 != 0) {
-                    for (int k = i; k <= j; k++) {
-                        ans += arr[k];
-                    }
-                }
-            }
+            int total = ((i + 1) * (n - i));
+            int odd = (total + 1) / 2;
+            ans += odd * arr[i];
         }
         return ans;
     }
