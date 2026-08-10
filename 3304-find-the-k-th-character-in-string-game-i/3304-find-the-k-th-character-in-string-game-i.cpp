0ec88a4f7354a9a006631e417107711fa5d47b8b
Krishna Tiwari <krishna.08072005@gmail.com>
2026-08-11 01:30:34 +0530

    Time: 0 ms (100%), Space: 7.9 MB (83.96%) - LeetHub

--- 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.cpp
+++ 3304-find-the-k-th-character-in-string-game-i/3304-find-the-k-th-character-in-string-game-i.cpp
@@ -1,16 +1,4 @@
 class Solution {
 public:
-    char kthCharacter(int k) {
-        string word = "a";
-        while (word.size() < k) {
-            word = word + gen(word);
-        }
-        return word[k - 1];
-    }
-    string gen(string s) {
-        for (int i = 0; i < s.size(); i++) {
-            s[i] = 'a' + ((s[i] - 'a') + 1) % 26;
-        }
-        return s;
-    }
+    char kthCharacter(int k) { return 'a' + __builtin_popcount(k - 1); }
 };
