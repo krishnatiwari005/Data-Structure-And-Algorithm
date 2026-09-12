d73d84a03b52e7660a2ae6410407f18977959c97
Krishna Tiwari <krishna.08072005@gmail.com>
2026-09-12 22:21:07 +0530

    Time: 70 ms (43.88%), Space: 71.8 MB (40.67%) - LeetHub

---
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        long long ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans +=
+                upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]) -
+                lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]);
+        }
+        return ans;
+    }
+};
