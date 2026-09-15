4815c141256cd7bf65f9c632b6ca44f3d78d6334
Krishna Tiwari <krishna.08072005@gmail.com>
2026-09-16 02:14:08 +0530

    Time: 0 ms (100%), Space: 7.8 MB (87.98%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n <= 3)
+            return n;
+        int prev = 3;
+        int prev2 = 2;
+        int cur = 0;
+        for (int i = 3; i < n; i++) {
+            cur = prev + prev2;
+            prev2 = prev;
+            prev = cur;
+        }
+        return cur;
+    }
+};
